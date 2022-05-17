17fb77611551243e7d53be78e3ea80aa1f0ed4f9
MariaK1506 <93434705+MariaK1506@users.noreply.github.com>
2022-05-17 22:04:43 +0300

    correct

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,6 +5,8 @@ const refs = {
   galleryList: document.querySelector(".gallery"),
 };
 
+// 1.Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи. Используй готовый код из первого задания.
+
 function createGalleryMarkup(items) {
   return items
     .map(({ preview, original, description }) => {
@@ -31,6 +33,8 @@ console.log(galleryItems);
 
 //   openModal();
 // }
+
+// 4. Посмотри в документации секцию «Options» и добавь отображение подписей к изображениям из атрибута alt. Пусть подпись будет снизу и появляется через 250 миллисекунд после открытия изображения.
 
 const options = {
   captions: true,
